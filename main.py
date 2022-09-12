dfd5f518fc02c69fd914e148df4b69318fc93b67
senπ <bagdi.rahul2004@gmail.com>
2022-09-12 16:35:59 +0530

    Added Playlist Download  Option

--- main.py
+++ main.py
@@ -3,56 +3,110 @@ import shutil
 import tempfile
 import time
 from pytube import YouTube
+from pytube import Playlist
 import ffmpeg
 
 
-print("YouTube Video Downloader [made by rahulbagdi]")
-print("Thank You for using my Software")
+print("YouTube Video Downloader [by codingsenpi]")
+print("Thank You for using my program :)")
 
-n = int(input("Enter number of videos : "))
+method = int(input("Enter 1 to Download Videos\nEnter 11 to Download Complete Playlist"))
 
-# Below line read inputs from user using map() function
-link = list(map(str, input("\nEnter the links : ").strip().split()))[:n]
+if method == 1:
+        
+    n = int(input("Enter number of videos : "))
 
-path = input("Enter the path where you want to save the video/s\ne.g.'{}' ".format(r"C:\\Users\\"))
+    # Below line read inputs from user using map() function
+    link = list(map(str, input("\nEnter the links : ").strip().split()))[:n]
 
-tempPath = tempfile.gettempdir()
-print("Got Temp Folder Successfully")
+    path = input("Enter the path where you want to save the video/s\ne.g.'{}' ".format(r"C:\\anyfolder\\"))
 
-resolution = input("Enter Resolution \n e.g '720p' , '1080p' ")
+    tempPath = tempfile.gettempdir()
+    print("Got Temp Folder Successfully")
 
-for i in link:
+    resolution = input("Enter Resolution \n e.g '720p' , '1080p' ")
 
-    # Deleting last downloaded files
-    if os.path.isdir(r"{}\\video".format(tempPath)):
-        shutil.rmtree("{}\\video".format(tempPath))
-    if os.path.isdir(r"{}\\audio".format(tempPath)):
-        shutil.rmtree("{}\\audio".format(tempPath))
+    for i in link:
 
-    yt = YouTube(i)
-    print("Downloading {}".format(yt.title))
-    time.sleep(2)
-    video = yt.streams.filter(res=resolution).first().download('{}\\video'.format(tempPath))
-    print("Got Video Stream Successfully")
-    # Renaming video to video.mp4
-    videoPath = r"{}\\video\\".format(tempPath)
-    videoName = os.listdir(r"{}\\video".format(tempfile.gettempdir()))[0]
-    os.rename("{}{}".format(videoPath, videoName), "{}video.mp4".format(videoPath))
-    time.sleep(2)
-    audio = yt.streams.filter(only_audio=True)
-    audio[0].download('{}\\audio'.format(tempPath))
-    print("Got Audio Stream Successfully")
-    # Renaming audio to audio.mp4
-    audioPath = r"{}\\audio\\".format(tempPath)
-    audioName = os.listdir(r"{}\\audio".format(tempfile.gettempdir()))[0]
-    os.rename("{}{}".format(audioPath, audioName), "{}audio.mp4".format(audioPath))
-    time.sleep(2)
-    print("Initializing Conversion: Combing Audio and Video")
-    audio_stream = ffmpeg.input('{}\\audio\\audio.mp4'.format(tempPath))
-    video_stream = ffmpeg.input('{}\\video\\video.mp4'.format(tempPath))
-    ffmpeg.output(audio_stream, video_stream, '{}{}.mp4'.format(path, yt.title)).run()
-    print("Deleting Temp Files")
-    time.sleep(2)
-    os.remove(r"{}\\video.mp4".format(videoPath))
-    os.remove(r"{}\\audio.mp4".format(audioPath))
+        # Deleting last downloaded files
+        if os.path.isdir(r"{}\\video".format(tempPath)):
+            shutil.rmtree("{}\\video".format(tempPath))
+        if os.path.isdir(r"{}\\audio".format(tempPath)):
+            shutil.rmtree("{}\\audio".format(tempPath))
+
+        yt = YouTube(i)
+        print("Downloading {}".format(yt.title))
+        time.sleep(2)
+        video = yt.streams.filter(res=resolution).first().download('{}\\video'.format(tempPath))
+        print("Got Video Stream Successfully")
+        # Renaming video to video.mp4
+        videoPath = r"{}\\video\\".format(tempPath)
+        videoName = os.listdir(r"{}\\video".format(tempfile.gettempdir()))[0]
+        os.rename("{}{}".format(videoPath, videoName), "{}video.mp4".format(videoPath))
+        time.sleep(2)
+        audio = yt.streams.filter(only_audio=True)
+        audio[0].download('{}\\audio'.format(tempPath))
+        print("Got Audio Stream Successfully")
+        # Renaming audio to audio.mp4
+        audioPath = r"{}\\audio\\".format(tempPath)
+        audioName = os.listdir(r"{}\\audio".format(tempfile.gettempdir()))[0]
+        os.rename("{}{}".format(audioPath, audioName), "{}audio.mp4".format(audioPath))
+        time.sleep(2)
+        print("Initializing Conversion: Combing Audio and Video")
+        audio_stream = ffmpeg.input('{}\\audio\\audio.mp4'.format(tempPath))
+        video_stream = ffmpeg.input('{}\\video\\video.mp4'.format(tempPath))
+        ffmpeg.output(audio_stream, video_stream, '{}{}.mp4'.format(path, yt.title)).run()
+        print("Deleting Temp Files")
+        time.sleep(2)
+        os.remove(r"{}\\video.mp4".format(videoPath))
+        os.remove(r"{}\\audio.mp4".format(audioPath))
+        print("Download Finished at {}".format(path))
+
+if method == 11:
+            
+    link = Playlist(input("Enter Playlist URL"))
+
+    path = input("Enter the path where you want to save the video/s\ne.g.'{}' ".format(r"C:\\anyfolder\\"))
+
+    tempPath = tempfile.gettempdir()
+    print("Got Temp Folder Successfully")
+
+    resolution = input("Enter Resolution \n e.g '720p' , '1080p' ")
+    print(link)
+    print(link.videos)
+    print(link.video_urls)
+    for i in link.video_urls:
+        print(i)
+        # Deleting last downloaded files
+        if os.path.isdir(r"{}\\video".format(tempPath)):
+            shutil.rmtree("{}\\video".format(tempPath))
+        if os.path.isdir(r"{}\\audio".format(tempPath)):
+            shutil.rmtree("{}\\audio".format(tempPath))
+
+        yt = YouTube(i)
+        print("Downloading {}".format(yt.title))
+        time.sleep(2)
+        video = yt.streams.filter(res=resolution).first().download('{}\\video'.format(tempPath))
+        print("Got Video Stream Successfully")
+        # Renaming video to video.mp4
+        videoPath = r"{}\\video\\".format(tempPath)
+        videoName = os.listdir(r"{}\\video".format(tempfile.gettempdir()))[0]
+        os.rename("{}{}".format(videoPath, videoName), "{}video.mp4".format(videoPath))
+        time.sleep(2)
+        audio = yt.streams.filter(only_audio=True)
+        audio[0].download('{}\\audio'.format(tempPath))
+        print("Got Audio Stream Successfully")
+        # Renaming audio to audio.mp4
+        audioPath = r"{}\\audio\\".format(tempPath)
+        audioName = os.listdir(r"{}\\audio".format(tempfile.gettempdir()))[0]
+        os.rename("{}{}".format(audioPath, audioName), "{}audio.mp4".format(audioPath))
+        time.sleep(2)
+        print("Initializing Conversion: Combing Audio and Video")
+        audio_stream = ffmpeg.input('{}\\audio\\audio.mp4'.format(tempPath))
+        video_stream = ffmpeg.input('{}\\video\\video.mp4'.format(tempPath))
+        ffmpeg.output(audio_stream, video_stream, '{}{}.mp4'.format(path, yt.title)).run()
+        print("Deleting Temp Files")
+        time.sleep(2)
+        os.remove(r"{}\\video.mp4".format(videoPath))
+        os.remove(r"{}\\audio.mp4".format(audioPath))
     print("Download Finished at {}".format(path))
